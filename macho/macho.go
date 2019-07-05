e0b686facb820aac61879da00d6b456cf415d8cf
Christian Roese <christian.roese@mortonbuildings.com>
2019-07-05 13:32:07 -0500

    Adds header reading code

--- macho/macho.go
+++ macho/macho.go
@@ -10,3 +10,40 @@ const (
 	// Cigam64 is the 64-bit magic number in little-endian
 	Cigam64 uint32 = 0xCFFAEDFE
 )
+
+type FileHeader struct {
+	Magic      uint32
+	CpuType    Cpu
+	CpuSubType uint32
+	Type       Type
+	Ncmds      uint32
+	SizeOfCmds uint32
+	Flags      uint32
+}
+
+const (
+	FileHeaderSize32 = 7 * 4 // 7 4-byte fields
+	FileHeaderSize64 = 8 * 4 // 8 4-byte fields (additional field is reserved)
+)
+
+type Cpu uint32
+
+const arch64 = 0x1000000
+
+const (
+	Cpu386   Cpu = 7
+	CpuAmd64 Cpu = (Cpu386 | arch64)
+	CpuArm   Cpu = 12
+	CpuArm64 Cpu = (CpuArm | arch64)
+	CpuPpc   Cpu = 18
+	CpuPpc64 Cpu = (CpuPpc | arch64)
+)
+
+type Type uint32
+
+const (
+	TypeObject     Type = 1
+	TypeExecutable Type = 2
+	TypeDylib      Type = 6
+	TypeBundle     Type = 8
+)
--- main.go
+++ main.go
@@ -30,23 +30,68 @@ func main() {
 }
 
 func dumpDetails(r io.ReaderAt) error {
+	sr := io.NewSectionReader(r, 0, 1<<63-1)
+
 	var magic [4]byte
 	if _, err := r.ReadAt(magic[0:], 0); err != nil {
 		return err
 	}
 
-	be := binary.BigEndian.Uint32(magic[0:])
-	le := binary.LittleEndian.Uint32(magic[0:])
+	combined := uint32(magic[0])<<24 | uint32(magic[1])<<16 |
+		uint32(magic[2])<<8 | uint32(magic[3])
 
-	switch {
-	case be == macho.Magic32:
+	var order binary.ByteOrder
+	is64 := false
+	switch combined {
+	case macho.Magic32:
 		fmt.Println("32-bit, big-endian")
-	case be == macho.Magic64:
+		order = binary.BigEndian
+	case macho.Magic64:
 		fmt.Println("64-bit, big-endian")
-	case le == macho.Cigam32:
+		is64 = true
+		order = binary.BigEndian
+	case macho.Cigam32:
 		fmt.Println("32-bit, little-endian")
-	case be == macho.Cigam64:
+		order = binary.LittleEndian
+	case macho.Cigam64:
 		fmt.Println("64-bit, little-endian")
+		is64 = true
+		order = binary.LittleEndian
+	default:
+		return fmt.Errorf("invalid magic number 0x%x", magic)
 	}
+
+	header := new(macho.FileHeader)
+	if err := binary.Read(sr, order, header); err != nil {
+		return err
+	}
+
+	switch header.CpuType {
+	case macho.Cpu386:
+		fmt.Println("CPU Type: x86")
+	case macho.CpuAmd64:
+		fmt.Println("CPU Type: x64")
+	}
+
+	switch header.Type {
+	case macho.TypeObject:
+		fmt.Println("relocatable object file")
+	case macho.TypeExecutable:
+		fmt.Println("executable file")
+	case macho.TypeDylib:
+		fmt.Println("dynamically bound shared library")
+	case macho.TypeBundle:
+		fmt.Println("dynamically bound bundle file")
+	}
+
+	fmt.Printf("%d load commands, totalling %d bytes\n",
+		header.Ncmds, header.SizeOfCmds)
+
+	offset := int64(macho.FileHeaderSize32)
+	if is64 {
+		offset = macho.FileHeaderSize64
+	}
+
+	fmt.Println(offset)
 	return nil
 }
